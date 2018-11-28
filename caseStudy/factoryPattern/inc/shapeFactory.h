c0c35e6515160614844c5499ea8efd6969b57cd6
zhangwb100611336 <466636087@qq.com>
2018-11-28 21:49:19 +0800

    add doxygen comment

--- caseStudy/factoryPattern/inc/shapeFactory.h
+++ caseStudy/factoryPattern/inc/shapeFactory.h
@@ -1,21 +1,41 @@
+/**
+ *	You should have recived a copy of the GNU general Public License
+ *	along with OST. if not,see <http://www.gun.org/licenses/>.
+ *	
+ *	@file       shapeFactory.h
+ *	@brief      Function implementation
+ *	@author     Wenbing.Zhang
+ *	@version    0.1
+ *	@date       2018.11.20
+ **/
+
 #include <shape.h>
 #include <string>
 #include <map>
 
-
+/**
+ *	@brief factory pattern namespace
+ */
 namespace factoryPattern
 {
 	typedef shape* (* CreatorFuntion)(void);
+	/**
+	 *	@brief class of shape factory \n
+	 *	The class is use to register shape and create shape instance
+	 */
 	class ShapeFactory
 	{
 	public：
 		shapeFactory();
 		~shapeFactory();
-		static ShapeFactory& init();
-		static bool shapeRegister(const char* str,CreatorFuntion pCreatorFuntion);
-		static getShape(const char* str);
+		/**
+		 *	@brief init 
+		 */
+		void init();
+		static bool shapeRegister(std::string str,CreatorFuntion pCreatorFuntion);
+		static sharp* getShape(std::string str);
 	private:
-		static map<string,shape*> shapeMap{};
-		static map<string,CreatorFuntion>  objMap{};
+		static map<std::string,shape*> shapeMap{};
+		static map<std::string,CreatorFuntion>  creatorMap{};
 	};
 }
--- caseStudy/factoryPattern/src/shapeFactory.cc
+++ caseStudy/factoryPattern/src/shapeFactory.cc
@@ -1,19 +1,39 @@
+/**
+ *	You should have recived a copy of the GNU general Public License
+ *	along with OST. if not,see <http://www.gun.org/licenses/>.
+ *	
+ *	@file       shapeFactory.cc
+ *	@brief      Function implementation
+ *	@author     Wenbing.Zhang
+ *	@version    0.1
+ *	@date       2018.11.20
+ **/
+
 #include "shapeFactory.h"
 #include <iostream>
 using namespace std;
 
 namespace factoryPattern
 {
-	shapeFactory::shapeFactory()
-	{
-	}
+		shapeFactory::shapeFactory()
+		{
+		}
 
-	shapeFactory::~shapeFactory()
-	{
-	}
+		shapeFactory::~shapeFactory()
+		{
+		}
 
-	bool shapeFactory::shapeRegister(const char* str,CreatorFuntion pCreatorFuntion)
-	{
-		if()
-	}
+
+		bool shapeFactory::shapeRegister(string str,CreatorFuntion pCreatorFuntion)
+		{
+				map<std::string,CreatorFuntion*>::iterator it ;
+				it= creatorMap.find(str);
+				if(it!=creatorMap.end())
+				{
+						cout << "shape "<< str << "have alrady registered" << endl;
+						return true;
+				}
+				creatorMap.insert(std::pair<std::string,CreatorFuntion> (str,pCreatorFuntion));
+				return true;
+		}
 }
